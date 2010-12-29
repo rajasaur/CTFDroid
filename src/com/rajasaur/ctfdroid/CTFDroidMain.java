cd934880afc5846e418a91e4bc06ad21699af18c
Raja Venkataraman <rajasaur@gmail.com>
2010-12-29 14:47:12 +0530

    Basic table layout for username/password to login to TeamForge. No functionality works at this point

---
+++ src/com/rajasaur/ctfdroid/CTFDroidMain.java
@@ -0,0 +1,13 @@
+package com.rajasaur.ctfdroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CTFDroidMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
